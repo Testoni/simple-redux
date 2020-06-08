1e8ba5f81d6fbd30d38d7cfb17b368120201a046
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-08 00:30:58 -0300

    interval component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 import Card from './components/Card'
+import Interval from './components/Interval'
 
 function App() {
   return (
     <div className="App">
       <h1>React-Redux (Simple)</h1>
       <div className='line'>
-        <Card title='CARD 1' red>x</Card>
-        <Card title='CARD 2' green>x</Card>
+        <Interval></Interval>
       </div>
       <div className='line'>
+        <Card title='CARD 2' green>x</Card>
         <Card title='CARD 3' blue>z</Card>
         <Card title='CARD 4' purple>z</Card>
       </div>
