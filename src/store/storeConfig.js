57929b25612b85d7962915e5c3340b2d7e308cc6
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-09 20:55:24 -0300

    configuring REDUX

---
+++ src/store/storeConfig.js
@@ -0,0 +1,23 @@
+import { createStore, combineReducers } from 'redux'
+
+const reducers = combineReducers({
+    numbers: function(state, action) {
+        return {
+            min: 1,
+            max: 10
+        }
+    },
+    names: function(state, action) {
+        return [
+            'Ana',
+            'Bia',
+            'Carlos'
+        ]
+    }
+})
+
+function storeConfig() {
+    return createStore(reducers)
+}
+
+export default storeConfig
