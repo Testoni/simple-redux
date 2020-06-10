9936d1e93681d3fd04ca2e0afea726cfb0a98b4c
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-09 21:50:05 -0300

    connect redux components

--- src/components/Average.jsx
+++ src/components/Average.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import Card from './Card'
 
-export default props => {
+function Average(props) {
+    const {min, max} = props
     return (
         <Card title='Numbers Average' green>
         <div>
             <span>
                 <span>Result:</span>
-                <strong>5</strong>
+                <strong>{(max + min) / 2}</strong>
             </span>
         </div>
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        min: state.numbers.min,
+        max: state.numbers.max
+    }
+}
+
+export default connect(mapStateToProps)(Average)
