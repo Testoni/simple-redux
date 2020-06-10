c4566c06b0d74f7510c74ec2f4361ed3fbf1afec
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-10 00:24:07 -0300

    actiontypes const

--- src/store/actions/numbers.js
+++ src/store/actions/numbers.js
@@ -1,15 +1,19 @@
+import {
+    NUM_MIN_CHANGED,
+    NUM_MAX_CHANGED
+} from './actionTypes'
 
 // action creater
 export function changeMinNumber(newNumber) {
     return {
-        type: 'NUM_MIN_CHANGED',
+        type: NUM_MIN_CHANGED,
         payload: newNumber
     }
 }
 // action creater
 export function changeMaxNumber(newNumber) {
     return {
-        type: 'NUM_MAX_CHANGED',
+        type: NUM_MAX_CHANGED,
         payload: newNumber
     }
 }
--- src/store/reducers/numbers.js
+++ src/store/reducers/numbers.js
@@ -1,3 +1,8 @@
+import {
+    NUM_MIN_CHANGED,
+    NUM_MAX_CHANGED
+} from '../actions/actionTypes'
+
 const initialState = {
     min: 1,
     max: 10
@@ -5,12 +10,12 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch (action.type) {
-        case 'NUM_MIN_CHANGED':
+        case NUM_MIN_CHANGED:
             return {
                 ...state,
                 min: action.payload
             }
-        case 'NUM_MAX_CHANGED':
+        case NUM_MAX_CHANGED:
             return {
                 ...state,
                 max: action.payload
